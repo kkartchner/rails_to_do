2c5ba6b412f09a0b88e3602a090676b8690521e3
Ky Kartchner <kartchnerky@gmail.com>
2020-05-18 13:58:03 -0600

    Add basic sign up and login functionality using devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
-  root 'to_dos#index'
+  devise_for :users
+  root to: 'pages#index'
 
   resources :to_dos do
     member do
       post 'toggle'
     end
   end
-
-  get 'signup', to: 'users#new'
-  resources :users, only: [:index]
-
-  get 'login', to: 'sessions#new'
-
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20200518035421_rename_first_name_column.rb
+++
@@ -1,5 +0,0 @@
-class RenameFirstNameColumn < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :users, :first_name, :name
-  end
-end
